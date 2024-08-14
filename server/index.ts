4c9a1077301ee5c5d3300a4a9f501c74e3a5a3ed
YaBoiJoshuaCardoso <144963353+YaBoiJoshuaCardoso@users.noreply.github.com>
2024-08-14 15:00:26 -0400

    fixing limit issues

--- server/index.ts
+++ server/index.ts
@@ -111,7 +111,8 @@ const port = process.env.PORT || 8000;
 // });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.static(path.join(__dirname, '../dist')));
 console.log(path.join(__dirname, '../dist'))
 
